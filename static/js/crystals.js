9335492516f19d3dbd83d273ff624e25b6d2dd76
J. A. Mercury <13936649+tsukinoooo@users.noreply.github.com>
2018-01-14 19:46:23 -0500

    Create crystals.js

---
+++ static/js/crystals.js
@@ -0,0 +1,74 @@
+var crystals = {
+  "wis": {
+    "name": "Wind Shard",
+    "ring": "843899bc8f6"
+  },
+  "is": {
+    "name": "Ice Shard",
+    "ring": "f2dfd367f1e"
+  },
+  "fs": {
+    "name": "Fire Shard",
+    "ring": "27623d06a42"
+  },
+  "es": {
+    "name": "Earth Shard",
+    "ring": "ef4394eb49b"
+  },
+  "ls": {
+    "name": "Lightning Shard",
+    "ring": "6fed6129996"
+  },
+  "was": {
+    "name": "Water Shard",
+    "ring": "9f967ad6b5a"
+  },
+  "wicr": {
+    "name": "Wind Crystal",
+    "ring": "5db117cd77b"
+  },
+  "icr": {
+    "name": "Ice Crystal",
+    "ring": "2d800a06851"
+  },
+  "fcr": {
+    "name": "Fire Crystal",
+    "ring": "75403105d7f"
+  },
+  "ecr": {
+    "name": "Earth Crystal",
+    "ring": "ff6eae7bdcb"
+  },
+  "lcr": {
+    "name": "Lightning Crystal",
+    "ring": "b21a951916e"
+  },
+  "wacr": {
+    "name": "Water Crystal",
+    "ring": "44d3f4cb26b"
+  },
+  "wicl": {
+    "name": "Wind Cluster",
+    "ring": "b61cbfd2d6e"
+  },
+  "icl": {
+    "name": "Ice Cluster",
+    "ring": "4dbe0ec0a76"
+  },
+  "fcl": {
+    "name": "Fire Cluster",
+    "ring": "e8ccb4b1f81"
+  },
+  "ecl": {
+    "name": "Earth Cluster",
+    "ring": "c97c6825890"
+  },
+  "lcl": {
+    "name": "Lightning Cluster",
+    "ring": "06061fd121b"
+  },
+  "wacl": {
+    "name": "Water Cluster",
+    "ring": "e3d82488f80"
+  }
+};
