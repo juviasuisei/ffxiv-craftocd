46df550462cd79f54fce644f37c7a8b1fb47b6a3
J. A. Mercury <13936649+tsukinoooo@users.noreply.github.com>
2018-01-14 19:44:57 -0500

    Update translate.js

--- static/js/translate.js
+++ static/js/translate.js
@@ -2,21 +2,37 @@ $('body').on('click', '#translate', function(event) {
   switch($('.active input').attr('id')) {
     case 'items':
       translateItems($('#data').val());
+      break;
+    case 'crystals':
+      translateItems($('#data').val());
+      break;
   }
 });
 
 function translateItems(data) {
-  var items = {};
+  var temp_items = {};
   $.each(data.split('\n'), function(k,r) {
     var col = r.split('\t');
-    items[col[0]] = {
+    temp_items[col[0]] = {
       'name' : col[1],
       'ring' : col[2],
       'fish' : col[3],
       'api' : col[4]
     }
   });
-  displayJSON(items);
+  displayJSON(temp_items);
+}
+
+function translateCrystals(data) {
+  var temp_crystals = {};
+  $.each(data.split('\n'), function(k,r) {
+    var col = r.split('\t');
+    temp_crystals[col[0]] = {
+      'name' : col[1],
+      'api' : col[4]
+    }
+  });
+  displayJSON(temp_crystals);
 }
 
 function displayJSON(data) {
