beefca2f050eb2187af90a0720963af7d062d58f
J. A. Mercury <13936649+tsukinoooo@users.noreply.github.com>
2018-01-14 21:13:49 -0500

    Create leves.js

---
+++ static/js/leves.js
@@ -0,0 +1,274 @@
+var leves = {
+  "T'mokkri": {
+    "1": {
+      "ARM": [
+        {
+          "qty": "3",
+          "item": "bi"
+        },
+        {
+          "qty": "1",
+          "item": "bp"
+        },
+        {
+          "qty": "1",
+          "item": "bsk"
+        }
+      ],
+      "BSM": [
+        {
+          "qty": "1",
+          "item": "bht"
+        },
+        {
+          "qty": "3",
+          "item": "bi"
+        },
+        {
+          "qty": "5",
+          "item": "brv"
+        }
+      ],
+      "CUL": [
+        {
+          "qty": "1",
+          "item": "be"
+        },
+        {
+          "qty": "1",
+          "item": "gt"
+        },
+        {
+          "qty": "9",
+          "item": "ts"
+        }
+      ]
+    },
+    "region": "La Noscea",
+    "map": "Limsa Lominsa Upper Decks",
+    "x": "11.0",
+    "y": "11.0"
+  },
+  "Wyrkholsk": {
+    "1": {
+      "ARM": [
+        {
+          "qty": "1",
+          "item": "ba"
+        },
+        {
+          "qty": "1",
+          "item": "bhpl"
+        },
+        {
+          "qty": "1",
+          "item": "brv"
+        }
+      ],
+      "BSM": [
+        {
+          "qty": "1",
+          "item": "bbs"
+        },
+        {
+          "qty": "1",
+          "item": "bsw"
+        },
+        {
+          "qty": "1",
+          "item": "bwa"
+        }
+      ],
+      "CUL": [
+        {
+          "qty": "5",
+          "item": "mpsy"
+        },
+        {
+          "qty": "1",
+          "item": "mms"
+        },
+        {
+          "qty": "3",
+          "item": "rs"
+        }
+      ]
+    },
+    "region": "La Noscea",
+    "map": "Lower La Noscea",
+    "x": "31.0",
+    "y": "20.0"
+  },
+  "Eustace": {
+    "1": {
+      "ALC": [
+        {
+          "qty": "3",
+          "item": "gfa"
+        },
+        {
+          "qty": "1",
+          "item": "lgm"
+        },
+        {
+          "qty": "3",
+          "item": "qs"
+        }
+      ],
+      "GSM": [
+        {
+          "qty": "1",
+          "item": "bbr"
+        },
+        {
+          "qty": "1",
+          "item": "ci"
+        },
+        {
+          "qty": "1",
+          "item": "cw"
+        }
+      ],
+      "WVR": [
+        {
+          "qty": "1",
+          "item": "hcs"
+        },
+        {
+          "qty": "1",
+          "item": "hup"
+        },
+        {
+          "qty": "1",
+          "item": "hus"
+        }
+      ]
+    },
+    "region": "Thanalan",
+    "map": "Uldah - Steps of Nald",
+    "x": "12.0",
+    "y": "9.1"
+  },
+  "Graceful Song": {
+    "1": {
+      "ALC": [
+        {
+          "qty": "3",
+          "item": "ag"
+        },
+        {
+          "qty": "3",
+          "item": "ad"
+        },
+        {
+          "qty": "1",
+          "item": "mpw"
+        }
+      ],
+      "GSM": [
+        {
+          "qty": "1",
+          "item": "bhr"
+        },
+        {
+          "qty": "1",
+          "item": "bst"
+        },
+        {
+          "qty": "5",
+          "item": "cr"
+        }
+      ],
+      "WVR": [
+        {
+          "qty": "1",
+          "item": "hcf"
+        },
+        {
+          "qty": "1",
+          "item": "hcl"
+        },
+        {
+          "qty": "1",
+          "item": "hh"
+        }
+      ]
+    },
+    "region": "Thanalan",
+    "map": "Western Thanalan",
+    "x": "26.0",
+    "y": "24.0"
+  },
+  "Muriaule": {
+    "1": {
+      "CRP": [
+        {
+          "qty": "3",
+          "item": "mplu"
+        },
+        {
+          "qty": "1",
+          "item": "mpsb"
+        },
+        {
+          "qty": "1",
+          "item": "smps"
+        }
+      ],
+      "LTW": [
+        {
+          "qty": "3",
+          "item": "l"
+        },
+        {
+          "qty": "1",
+          "item": "lch"
+        },
+        {
+          "qty": "1",
+          "item": "mp"
+        }
+      ]
+    },
+    "region": "The Black Shroud",
+    "map": "Central Shroud",
+    "x": "23.0",
+    "y": "19.0"
+  },
+  "Gontrat": {
+    "1": {
+      "CRP": [
+        {
+          "qty": "1",
+          "item": "bhpn"
+        },
+        {
+          "qty": "1",
+          "item": "mpcl"
+        },
+        {
+          "qty": "1",
+          "item": "mppt"
+        }
+      ],
+      "LTW": [
+        {
+          "qty": "1",
+          "item": "lca"
+        },
+        {
+          "qty": "1",
+          "item": "ld"
+        },
+        {
+          "qty": "1",
+          "item": "lw"
+        }
+      ]
+    },
+    "region": "The Black Shroud",
+    "map": "New Gridania",
+    "x": "11.0",
+    "y": "13.0"
+  }
+};
