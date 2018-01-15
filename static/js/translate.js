c87dc28c67844ba9608f7c934680d915a3c9e0f2
J. A. Mercury <13936649+tsukinoooo@users.noreply.github.com>
2018-01-14 19:31:01 -0500

    Update translate.js

--- static/js/translate.js
+++ static/js/translate.js
@@ -1,4 +1,20 @@
 $('body').on('click', '#translate', function(event) {
-  console.log($('.active input').attr('id'));
-  console.log($('#data').val());
+  switch($('.active input').attr('id')) {
+    case 'items':
+      translateItems($('#data').val());
+  }
 });
+
+function translateItems(data) {
+  var items = {};
+  $.each(data.split('\n'), function(k,r) {
+    var col = r.split('\t');
+    items[col[0]] = {
+      'name' : col[1],
+      'ring' : col[2],
+      'fish' : col[3],
+      'api' : col[4]
+    }
+  });
+  console.log(items);
+}
