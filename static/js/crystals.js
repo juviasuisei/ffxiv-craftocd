bb73679b0480c35d6cbf62a04ac7e71f9fdf2583
J. A. Mercury <13936649+tsukinoooo@users.noreply.github.com>
2018-01-14 19:50:56 -0500

    Update crystals.js

--- static/js/crystals.js
+++ static/js/crystals.js
@@ -1,74 +1,74 @@
-var crystals = {
+var {
   "wis": {
     "name": "Wind Shard",
-    "ring": "843899bc8f6"
+    "api": "843899bc8f6"
   },
   "is": {
     "name": "Ice Shard",
-    "ring": "f2dfd367f1e"
+    "api": "f2dfd367f1e"
   },
   "fs": {
     "name": "Fire Shard",
-    "ring": "27623d06a42"
+    "api": "27623d06a42"
   },
   "es": {
     "name": "Earth Shard",
-    "ring": "ef4394eb49b"
+    "api": "ef4394eb49b"
   },
   "ls": {
     "name": "Lightning Shard",
-    "ring": "6fed6129996"
+    "api": "6fed6129996"
   },
   "was": {
     "name": "Water Shard",
-    "ring": "9f967ad6b5a"
+    "api": "9f967ad6b5a"
   },
   "wicr": {
     "name": "Wind Crystal",
-    "ring": "5db117cd77b"
+    "api": "5db117cd77b"
   },
   "icr": {
     "name": "Ice Crystal",
-    "ring": "2d800a06851"
+    "api": "2d800a06851"
   },
   "fcr": {
     "name": "Fire Crystal",
-    "ring": "75403105d7f"
+    "api": "75403105d7f"
   },
   "ecr": {
     "name": "Earth Crystal",
-    "ring": "ff6eae7bdcb"
+    "api": "ff6eae7bdcb"
   },
   "lcr": {
     "name": "Lightning Crystal",
-    "ring": "b21a951916e"
+    "api": "b21a951916e"
   },
   "wacr": {
     "name": "Water Crystal",
-    "ring": "44d3f4cb26b"
+    "api": "44d3f4cb26b"
   },
   "wicl": {
     "name": "Wind Cluster",
-    "ring": "b61cbfd2d6e"
+    "api": "b61cbfd2d6e"
   },
   "icl": {
     "name": "Ice Cluster",
-    "ring": "4dbe0ec0a76"
+    "api": "4dbe0ec0a76"
   },
   "fcl": {
     "name": "Fire Cluster",
-    "ring": "e8ccb4b1f81"
+    "api": "e8ccb4b1f81"
   },
   "ecl": {
     "name": "Earth Cluster",
-    "ring": "c97c6825890"
+    "api": "c97c6825890"
   },
   "lcl": {
     "name": "Lightning Cluster",
-    "ring": "06061fd121b"
+    "api": "06061fd121b"
   },
   "wacl": {
     "name": "Water Cluster",
-    "ring": "e3d82488f80"
+    "api": "e3d82488f80"
   }
 };
