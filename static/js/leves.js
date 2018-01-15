8e372ab9f5beb000dcc1cf44818e82e2b6e0ed9b
J. A. Mercury <13936649+tsukinoooo@users.noreply.github.com>
2018-01-15 00:04:21 -0500

    Update leves.js

--- static/js/leves.js
+++ static/js/leves.js
@@ -1,48 +1,50 @@
 var leves = {
   "T'mokkri": {
-    "1": {
-      "ARM": [
-        {
-          "qty": "3",
-          "item": "bi"
-        },
-        {
-          "qty": "1",
-          "item": "bp"
-        },
-        {
-          "qty": "1",
-          "item": "bsk"
-        }
-      ],
-      "BSM": [
-        {
-          "qty": "1",
-          "item": "bht"
-        },
-        {
-          "qty": "3",
-          "item": "bi"
-        },
-        {
-          "qty": "5",
-          "item": "brv"
-        }
-      ],
-      "CUL": [
-        {
-          "qty": "1",
-          "item": "be"
-        },
-        {
-          "qty": "1",
-          "item": "gt"
-        },
-        {
-          "qty": "9",
-          "item": "ts"
-        }
-      ]
+    "items": {
+      "1": {
+        "ARM": [
+          {
+            "qty": "3",
+            "item": "bi"
+          },
+          {
+            "qty": "1",
+            "item": "bp"
+          },
+          {
+            "qty": "1",
+            "item": "bsk"
+          }
+        ],
+        "BSM": [
+          {
+            "qty": "1",
+            "item": "bht"
+          },
+          {
+            "qty": "3",
+            "item": "bi"
+          },
+          {
+            "qty": "5",
+            "item": "brv"
+          }
+        ],
+        "CUL": [
+          {
+            "qty": "1",
+            "item": "be"
+          },
+          {
+            "qty": "1",
+            "item": "gt"
+          },
+          {
+            "qty": "9",
+            "item": "ts"
+          }
+        ]
+      }
     },
     "region": "La Noscea",
     "map": "Limsa Lominsa Upper Decks",
@@ -50,49 +52,51 @@ var leves = {
     "y": "11.0"
   },
   "Wyrkholsk": {
-    "1": {
-      "ARM": [
-        {
-          "qty": "1",
-          "item": "ba"
-        },
-        {
-          "qty": "1",
-          "item": "bhpl"
-        },
-        {
-          "qty": "1",
-          "item": "brv"
-        }
-      ],
-      "BSM": [
-        {
-          "qty": "1",
-          "item": "bbs"
-        },
-        {
-          "qty": "1",
-          "item": "bsw"
-        },
-        {
-          "qty": "1",
-          "item": "bwa"
-        }
-      ],
-      "CUL": [
-        {
-          "qty": "5",
-          "item": "mpsy"
-        },
-        {
-          "qty": "1",
-          "item": "mms"
-        },
-        {
-          "qty": "3",
-          "item": "rs"
-        }
-      ]
+    "items": {
+      "1": {
+        "ARM": [
+          {
+            "qty": "1",
+            "item": "ba"
+          },
+          {
+            "qty": "1",
+            "item": "bhpl"
+          },
+          {
+            "qty": "1",
+            "item": "brv"
+          }
+        ],
+        "BSM": [
+          {
+            "qty": "1",
+            "item": "bbs"
+          },
+          {
+            "qty": "1",
+            "item": "bsw"
+          },
+          {
+            "qty": "1",
+            "item": "bwa"
+          }
+        ],
+        "CUL": [
+          {
+            "qty": "5",
+            "item": "mpsy"
+          },
+          {
+            "qty": "1",
+            "item": "mms"
+          },
+          {
+            "qty": "3",
+            "item": "rs"
+          }
+        ]
+      }
     },
     "region": "La Noscea",
     "map": "Lower La Noscea",
@@ -100,49 +104,51 @@ var leves = {
     "y": "20.0"
   },
   "Eustace": {
-    "1": {
-      "ALC": [
-        {
-          "qty": "3",
-          "item": "gfa"
-        },
-        {
-          "qty": "1",
-          "item": "lgm"
-        },
-        {
-          "qty": "3",
-          "item": "qs"
-        }
-      ],
-      "GSM": [
-        {
-          "qty": "1",
-          "item": "bbr"
-        },
-        {
-          "qty": "1",
-          "item": "ci"
-        },
-        {
-          "qty": "1",
-          "item": "cw"
-        }
-      ],
-      "WVR": [
-        {
-          "qty": "1",
-          "item": "hcs"
-        },
-        {
-          "qty": "1",
-          "item": "hup"
-        },
-        {
-          "qty": "1",
-          "item": "hus"
-        }
-      ]
+    "items": {
+      "1": {
+        "ALC": [
+          {
+            "qty": "3",
+            "item": "gfa"
+          },
+          {
+            "qty": "1",
+            "item": "lgm"
+          },
+          {
+            "qty": "3",
+            "item": "qs"
+          }
+        ],
+        "GSM": [
+          {
+            "qty": "1",
+            "item": "bbr"
+          },
+          {
+            "qty": "1",
+            "item": "ci"
+          },
+          {
+            "qty": "1",
+            "item": "cw"
+          }
+        ],
+        "WVR": [
+          {
+            "qty": "1",
+            "item": "hcs"
+          },
+          {
+            "qty": "1",
+            "item": "hup"
+          },
+          {
+            "qty": "1",
+            "item": "hus"
+          }
+        ]
+      }
     },
     "region": "Thanalan",
     "map": "Uldah - Steps of Nald",
@@ -150,49 +156,51 @@ var leves = {
     "y": "9.1"
   },
   "Graceful Song": {
-    "1": {
-      "ALC": [
-        {
-          "qty": "3",
-          "item": "ag"
-        },
-        {
-          "qty": "3",
-          "item": "ad"
-        },
-        {
-          "qty": "1",
-          "item": "mpw"
-        }
-      ],
-      "GSM": [
-        {
-          "qty": "1",
-          "item": "bhr"
-        },
-        {
-          "qty": "1",
-          "item": "bst"
-        },
-        {
-          "qty": "5",
-          "item": "cr"
-        }
-      ],
-      "WVR": [
-        {
-          "qty": "1",
-          "item": "hcf"
-        },
-        {
-          "qty": "1",
-          "item": "hcl"
-        },
-        {
-          "qty": "1",
-          "item": "hh"
-        }
-      ]
+    "items": {
+      "1": {
+        "ALC": [
+          {
+            "qty": "3",
+            "item": "ag"
+          },
+          {
+            "qty": "3",
+            "item": "ad"
+          },
+          {
+            "qty": "1",
+            "item": "mpw"
+          }
+        ],
+        "GSM": [
+          {
+            "qty": "1",
+            "item": "bhr"
+          },
+          {
+            "qty": "1",
+            "item": "bst"
+          },
+          {
+            "qty": "5",
+            "item": "cr"
+          }
+        ],
+        "WVR": [
+          {
+            "qty": "1",
+            "item": "hcf"
+          },
+          {
+            "qty": "1",
+            "item": "hcl"
+          },
+          {
+            "qty": "1",
+            "item": "hh"
+          }
+        ]
+      }
     },
     "region": "Thanalan",
     "map": "Western Thanalan",
@@ -200,35 +208,37 @@ var leves = {
     "y": "24.0"
   },
   "Muriaule": {
-    "1": {
-      "CRP": [
-        {
-          "qty": "3",
-          "item": "mplu"
-        },
-        {
-          "qty": "1",
-          "item": "mpsb"
-        },
-        {
-          "qty": "1",
-          "item": "smps"
-        }
-      ],
-      "LTW": [
-        {
-          "qty": "3",
-          "item": "l"
-        },
-        {
-          "qty": "1",
-          "item": "lch"
-        },
-        {
-          "qty": "1",
-          "item": "mp"
-        }
-      ]
+    "items": {
+      "1": {
+        "CRP": [
+          {
+            "qty": "3",
+            "item": "mplu"
+          },
+          {
+            "qty": "1",
+            "item": "mpsb"
+          },
+          {
+            "qty": "1",
+            "item": "smps"
+          }
+        ],
+        "LTW": [
+          {
+            "qty": "3",
+            "item": "l"
+          },
+          {
+            "qty": "1",
+            "item": "lch"
+          },
+          {
+            "qty": "1",
+            "item": "mp"
+          }
+        ]
+      }
     },
     "region": "The Black Shroud",
     "map": "Central Shroud",
@@ -236,35 +246,37 @@ var leves = {
     "y": "19.0"
   },
   "Gontrat": {
-    "1": {
-      "CRP": [
-        {
-          "qty": "1",
-          "item": "bhpn"
-        },
-        {
-          "qty": "1",
-          "item": "mpcl"
-        },
-        {
-          "qty": "1",
-          "item": "mppt"
-        }
-      ],
-      "LTW": [
-        {
-          "qty": "1",
-          "item": "lca"
-        },
-        {
-          "qty": "1",
-          "item": "ld"
-        },
-        {
-          "qty": "1",
-          "item": "lw"
-        }
-      ]
+    "items": {
+      "1": {
+        "CRP": [
+          {
+            "qty": "1",
+            "item": "bhpn"
+          },
+          {
+            "qty": "1",
+            "item": "mpcl"
+          },
+          {
+            "qty": "1",
+            "item": "mppt"
+          }
+        ],
+        "LTW": [
+          {
+            "qty": "1",
+            "item": "lca"
+          },
+          {
+            "qty": "1",
+            "item": "ld"
+          },
+          {
+            "qty": "1",
+            "item": "lw"
+          }
+        ]
+      }
     },
     "region": "The Black Shroud",
     "map": "New Gridania",
