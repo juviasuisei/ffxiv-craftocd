8eed7fe5f51b2f80bc796825f1657fe3a32e0beb
J. A. Mercury <13936649+tsukinoooo@users.noreply.github.com>
2018-01-14 23:04:32 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,14 @@
-
+$('body').on('click', 'ul li a', function(event) {
+  var tab = 'lvl' + $(this).attr('id').substr(3);
+  rows = ''
+  $.each(logs, function(k,v) {
+    $.each(v, function(k2,v2) {
+      rows += '<tr>';
+      rows += '<td>' + k + '</td>';
+      rows += '<td>' + v2.level + '</td>';
+      rows += '<td>' + items[k2].name + '</td>';
+      rows += '</tr>';
+    });
+  });
+  $('#' + tab +
+});
