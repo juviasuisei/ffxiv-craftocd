24ff32a455f4dd9c56fb94b62886bac2ed458ac7
J. A. Mercury <13936649+tsukinoooo@users.noreply.github.com>
2018-02-04 12:45:34 -0500

    Update translate.js

--- static/js/translate.js
+++ static/js/translate.js
@@ -101,7 +101,7 @@ function translateNPCs(data) {
       'map' : col[2],
       'x' : col[3],
       'y' : col[4],
-      'items' : findSeries(col[5], col[6], col[7], col[8], col[9], col[10], col[11], col[12], col[13], col[14]) // O
+      'items' : findSeries(col[5], col[6], col[7], col[8], col[9], col[10], col[11], col[12], col[13], col[14], col[15]) // P
     }
   });
   displayJSON(temp_npcs);
